afa916414a4a2b22f5c374fa58a75d2a43b27845
sun <sun@suntekiMacBook-Air.local>
2013-07-27 00:00:15 +0800

    Initial Commit

---
+++ NCSearchPreference/NCSearchPreferenceController.h
@@ -0,0 +1,23 @@
+//
+//  NCSearchPreferenceController.h
+//  NCSearchPreference
+//
+//  Created by sun on 13-7-27.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Preferences/Preferences.h>
+
+@interface NCSearchPreferenceController : PSListController
+{
+}
+
+- (id)getValueForSpecifier:(PSSpecifier*)specifier;
+- (void)setValue:(id)value forSpecifier:(PSSpecifier*)specifier;
+- (void)followOnTwitter:(PSSpecifier*)specifier;
+- (void)visitWebSite:(PSSpecifier*)specifier;
+- (void)makeDonation:(PSSpecifier*)specifier;
+
+@end
